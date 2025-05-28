d76f530bf7e6d449c211701acf8735fe740f88f5
Michael Weiser <michael.weiser@eviden.com>
2025-05-28 09:45:05 +0200

    Add empty chef structure

---
+++ specs/default/chef/roles/local_execute_role.rb
@@ -0,0 +1,3 @@
+name "local_execute_role"
+description "Local changes to execute nodes"
+#run_list("recipe[local_execute::]")
---
+++ specs/default/chef/site-cookbooks/local_execute/README.md
@@ -0,0 +1,4 @@
+# local\_execute
+
+TODO: Enter the cookbook description here.
+
